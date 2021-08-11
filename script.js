71166a65fd713f3275d8c2496abb8447055a6cc3
Zect <57027411+sas08@users.noreply.github.com>
2021-08-11 16:49:32 +0900

    [Update]: いらないjsのコメントアウト
    - jsのclickEventが発火してくれない

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    // apiKey: "AIzaSyDnO__MK337j_rAqCxv8rTv2gC10NtvGQ0",
-    // authDomain: "chatapp-9fbb3.firebaseapp.com",
-    // projectId: "chatapp-9fbb3",
-    // storageBucket: "chatapp-9fbb3.appspot.com",
-    // messagingSenderId: "911276201672",
-    // appId: "1:911276201672:web:87604cb80decb51f11f7fd"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
 
-// const username = prompt("What's your name?");
+// document.getElementById("send-message").addEventListener("submit", postChat);
+// function postChat(e) {
+//   e.preventDefault();
+//   const timestamp = Date.now();
+//   const question = document.getElementById("question");
+//   const answer = document.getElementById("answer");
+//   const level = document.getElementById("level");
+//   const text = document.getElementById("text");
+//   const Que = question.value;
+//   const Ans = answer.value;
+//   const Lev = level.value;
+//   const Txt = text.value;
+//   question.value = "";
+//   answer.value = "";
+//   level.value = "";
+//   text.value = "";
+//   db.ref("questions/" + timestamp).set({
+//     qu: Que,
+//     an: Ans,
+//     le: Lev,
+//     tx: Txt,
+//   });
+// }
 
-document.getElementById("send-message").addEventListener("submit", postChat);
-function postChat(e) {
-  e.preventDefault();
-  const timestamp = Date.now();
-  const question = document.getElementById("question");
-  const answer = document.getElementById("answer");
-  const level = document.getElementById("level");
-  const text = document.getElementById("text");
-  const Que = question.value;
-  const Ans = answer.value;
-  const Lev = level.value;
-  const Txt = text.value;
-  question.value = "";
-  answer.value = "";
-  level.value = "";
-  text.value = "";
-  db.ref("questions/" + timestamp).set({
-    qu: Que,
-    an: Ans,
-    le: Lev,
-    tx: Txt,
-  });
-}
-
-const fetchChat = db.ref("messages/");
+// const fetchChat = db.ref("messages/");
+// // fetchChat.on("child_added", function (snapshot) {
+// //   const messages = snapshot.val();
+// //   const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
+// //   document.getElementById("messages").innerHTML += msg;
+// // });
 // fetchChat.on("child_added", function (snapshot) {
 //   const messages = snapshot.val();
-//   const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
-//   document.getElementById("messages").innerHTML += msg;
+//   const message = `<li class=${
+//     username === messages.username ? "sent" : "receive"
+//   }><span>${messages.usr}: </span>${messages.msg}</li>`;
+//   // append the message on the page
+//   document.getElementById("messages").innerHTML += message;
 // });
-fetchChat.on("child_added", function (snapshot) {
-  const messages = snapshot.val();
-  const message = `<li class=${
-    username === messages.username ? "sent" : "receive"
-  }><span>${messages.usr}: </span>${messages.msg}</li>`;
-  // append the message on the page
-  document.getElementById("messages").innerHTML += message;
-});
+
+
+// document.getElementById("book").addEventListener("click", showBook);
+function showBook() {
+  // bookContent = `<div class="bookContent"><p class="bookTitle">使用公式</p><div class="bookMain">2次方程式 $ax^2 + bx + c =0$ の根の公式は\[x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}\]</div>" `
+  // document.getElementById("content").innerHTML = bookContent;
+  alert('Click');
+}
+
+
+
+// mainContent = `<p>hello</p>`
+
+// document.getElementById("content").innerHTML = mainContent;
+
+
+
+
+function butotnClick(){
+  alert('Click');
+}
+
+onload=function() {
+  let bookButton = document.getElementById('book');
+  bookButton.onclick = showBook;
+  
+  let testButton = document.getElementById('xxx');
+  testButton.onclick = butotnClick;
+
+}
